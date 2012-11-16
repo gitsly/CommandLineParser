05b35d40e5438000eec3703fb5d714338b2ab73d
Martin <martin.collberg@gmail.com>
2012-11-16 13:23:29 +0100

    as run in release branch at work

--- CommandLineParser/Parser.cs
+++ CommandLineParser/Parser.cs
@@ -1,207 +1,136 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
-using System.Collections.Specialized;
 using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace CommandLineParser
 {
-
-    /* C# Regular Expressions Cheat Sheet
- 
-    \	Marks the next character as either a special character or escapes a literal. For example, "n" matches the character "n". "\n" matches a newline character. The sequence "\\" matches "\" and "\(" matches "(". Note: double quotes may be escaped by doubling them: "<a href=""...>"
-    ^	Depending on whether the MultiLine option is set, matches the position before the first character in a line, or the first character in the string.
-    $	Depending on whether the MultiLine option is set, matches the position after the last character in a line, or the last character in the string.
-    *	Matches the preceding character zero or more times. For example, "zo*" matches either "z" or "zoo".
-    +	Matches the preceding character one or more times. For example, "zo+" matches "zoo" but not "z".
-    ?	Matches the preceding character zero or one time. For example, "a?ve?" matches the "ve" in "never".
-    .	Matches any single character except a newline character.
-    (pattern)	Matches pattern and remembers the match. The matched substring can be retrieved from the resulting Matches collection, using Item [0]...[n]. To match parentheses characters ( ), use "\(" or "\)".
-    (?<name>pattern)	Matches pattern and gives the match a name.
-    (?:pattern)	A non-capturing group
-    (?=...)	A positive lookahead
-    (?!...)	A negative lookahead
-    (?<=...)	A positive lookbehind .
-    (?<!...)	A negative lookbehind .
-    x|y	Matches either x or y. For example, "z|wood" matches "z" or "wood". "(z|w)oo" matches "zoo" or "wood".
-    {n}	n is a non-negative integer. Matches exactly n times. For example, "o{2}" does not match the "o" in "Bob," but matches the first two o's in "foooood".
-    {n,}	n is a non-negative integer. Matches at least n times. For example, "o{2,}" does not match the "o" in "Bob" and matches all the o's in "foooood." "o{1,}" is equivalent to "o+". "o{0,}" is equivalent to "o*".
-    {n,m}	m and n are non-negative integers. Matches at least n and at most m times. For example, "o{1,3}" matches the first three o's in "fooooood." "o{0,1}" is equivalent to "o?".
-    [xyz]	A character set. Matches any one of the enclosed characters. For example, "[abc]" matches the "a" in "plain".
-    [^xyz]	A negative character set. Matches any character not enclosed. For example, "[^abc]" matches the "p" in "plain".
-    [a-z]	A range of characters. Matches any character in the specified range. For example, "[a-z]" matches any lowercase alphabetic character in the range "a" through "z".
-    [^m-z]	A negative range characters. Matches any character not in the specified range. For example, "[m-z]" matches any character not in the range "m" through "z".
-    \b	Matches a word boundary, that is, the position between a word and a space. For example, "er\b" matches the "er" in "never" but not the "er" in "verb".
-    \B	Matches a non-word boundary. "ea*r\B" matches the "ear" in "never early".
-    \d	Matches a digit character. Equivalent to [0-9].
-    \D	Matches a non-digit character. Equivalent to [^0-9].
-    \f	Matches a form-feed character.
-    \k	A back-reference to a named group.
-    \n	Matches a newline character.
-    \r	Matches a carriage return character.
-    \s	Matches any white space including space, tab, form-feed, etc. Equivalent to "[ \f\n\r\t\v]".
-    \S	Matches any nonwhite space character. Equivalent to "[^ \f\n\r\t\v]".
-    \t	Matches a tab character.
-    \v	Matches a vertical tab character.
-    \w	Matches any word character including underscore. Equivalent to "[A-Za-z0-9_]".
-    \W	Matches any non-word character. Equivalent to "[^A-Za-z0-9_]".
-    \num	Matches num, where num is a positive integer. A reference back to remembered matches. For example, "(.)\1" matches two consecutive identical characters.
-    \n	Matches n, where n is an octal escape value. Octal escape values must be 1, 2, or 3 digits long. For example, "\11" and "\011" both match a tab character. "\0011" is the equivalent of "\001" & "1". Octal escape values must not exceed 256. If they do, only the first two digits comprise the expression. Allows ASCII codes to be used in regular expressions.
-    \xn	Matches n, where n is a hexadecimal escape value. Hexadecimal escape values must be exactly two digits long. For example, "\x41" matches "A". "\x041" is equivalent to "\x04" & "1". Allows ASCII codes to be used in regular expressions.
-    \un	Matches a Unicode character expressed in hexadecimal notation with exactly four numeric digits. "\u0200" matches a space character.
-    \A	Matches the position before the first character in a string. Not affected by the MultiLine setting
-    \Z	Matches the position after the last character of a string. Not affected by the MultiLine setting.
-    \G	Specifies that the matches must be consecutive, without any intervening non-matching characters. 
-    */
-
-    
-
     public class Parser
     {
-        private Dictionary<string, string> ParsedArguments;
-        private Dictionary<string, PropertyInfo> Parameters;
 
-        public enum Token
+        public class CommandLinePropertySetEventArgs : EventArgs
         {
-            Identifier,
-            String,
-            Number,
-            Separator
+            public PropertyInfo Property { get; set; }
+            public Object Value { get; set; }
+
+            public CommandLinePropertySetEventArgs(PropertyInfo property, Object value)
+            {
+                Property = property;
+                Value = value;
+            }
         }
 
-        public List<Tuple<string, Token>> Tokenize(string commandLine)
+        public EventHandler<CommandLinePropertySetEventArgs> PropertySetByCommandLine;
+
+        private void OnPropertySetByCommandLine(PropertyInfo property, Object value)
         {
-            var tokenRegex = new Dictionary<Token, Regex>();
-            tokenRegex.Add(Token.Identifier, new Regex(@"^([a-z|_][a-z|0-9]*)", RegexOptions.IgnoreCase));
-            tokenRegex.Add(Token.String, new Regex(@"^""(.*)""|^'(.*)'"));
-            tokenRegex.Add(Token.Number, new Regex(@"^([0-9]+[,.]?[0-9]+)", RegexOptions.IgnoreCase));
-            tokenRegex.Add(Token.Separator, new Regex(@"^(-{1,2}| |[=:])", RegexOptions.IgnoreCase));
-
-            var tokens = new List<Tuple<string, Token>>();
-
-            var reminder = commandLine.Trim();
-            while (reminder.Length > 0)
+            var eventHandler = PropertySetByCommandLine;
+            if (eventHandler != null)
             {
-                // Try match valid tokens.
-                Match match = null;
-                foreach (var token in tokenRegex)
+                eventHandler(this, new CommandLinePropertySetEventArgs(property, value));
+            }
+        }
+
+        public Dictionary<string, string> ParseArguments(string[] args, Dictionary<string, PropertyInfo> parameterTypes)
+        {
+            var identifierRegex = new Regex(@"--([a-z][a-z|0-9]*)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+
+            var parameters = new Dictionary<string, string>();
+
+            string argumentString = string.Empty;
+            foreach (var arg in args)
+            {
+                argumentString += arg + " ";
+            }
+
+            var remainder = argumentString.Trim();
+
+
+            string param = null;
+            while (remainder.Length > 0)
+            {
+                if (param == null) // Find a --identifier
                 {
-                    match = token.Value.Match(reminder);
+                    var match = identifierRegex.Match(remainder);
                     if (match.Success)
                     {
-                        tokens.Add(new Tuple<string, Token>(match.Value, token.Key));
-                        reminder = reminder.Substring(match.Length);
-                        break;
+                        param = match.Groups[1].Value;
+                        remainder = remainder.Substring(match.Length).Trim();
                     }
                 }
-                if (match == null || !match.Success)
+                else
                 {
-                    throw new Exception(String.Format("Invalid token: '{0}'", reminder));
-                }
-            }
+                    var match = identifierRegex.Match(remainder);
+                    if (match.Success)
+                    {
+                        var value = remainder.Substring(0, match.Index);
 
-            return tokens;
-        }
-
-        private void ParseArguments(string[] args)
-        {
-            ParsedArguments = new Dictionary<string, string>();
-            Regex splitter = new Regex(@"^-{1,2}|^/|=|:| ", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-            Regex remover = new Regex(@"^['""]?(.*?)['""]?$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-
-            string Parameter = null;
-            string[] Parts;
-
-            // Valid parameters forms:
-            // {-,/,--}param{ ,=,:}((",')value(",'))
-            // Examples: 
-            // -param1 value1 --param2 /param3:"Test-:-work" 
-            //   /param4=happy -param5 '--=nice=--'
-            foreach (string argument in args)
-            {
-                var arg = argument.Trim();
-
-                // Look for new parameters (-,/ or --) and a
-                // possible enclosed value (=,:)
-                Parts = splitter.Split(arg, 3);
-
-
-                switch (Parts.Length)
-                {
-                    // Found a value (for the last parameter 
-                    // found (space separator))
-                    case 1:
-                        if (Parameter != null)
+                        if (match.Index == 0)
                         {
-                            if (!ParsedArguments.ContainsKey(Parameter))
+                            if (parameterTypes[param].PropertyType == typeof(bool))
                             {
-                                Parts[0] = remover.Replace(Parts[0], "$1");
-                                ParsedArguments.Add(Parameter, Parts[0]);
+                                parameters.Add(param, true.ToString());
                             }
-                            Parameter = null;
+                            else if (parameterTypes[param].PropertyType == typeof(string))
+                            {
+                                parameters.Add(param, "");
+                            }
                         }
-                        // else Error: no parameter waiting for a value (skipped)
-                        break;
-                    case 2: // Found just a parameter
-                        // The last parameter is still waiting. 
-                        // With no value, default to boolean 'true'
-                        if (Parameter != null)
+                        else
                         {
-                            if (!ParsedArguments.ContainsKey(Parameter))
-                                ParsedArguments.Add(Parameter, true.ToString());
+                            parameters.Add(param, value.Trim());
+                            remainder = remainder.Substring(value.Length, remainder.Length - value.Length);
                         }
-                        Parameter = Parts[1];
-                        break;
-
-                    // Parameter with enclosed value
-                    case 3:
-                        // The last parameter is still waiting. 
-                        // With no value, set it to true.
-                        if (Parameter != null)
-                        {
-                            if (!ParsedArguments.ContainsKey(Parameter))
-                                ParsedArguments.Add(Parameter, true.ToString());
-                        }
-                        Parameter = Parts[1];
-
-                        // Remove possible enclosing characters (",')
-                        if (!ParsedArguments.ContainsKey(Parameter))
-                        {
-                            Parts[2] = remover.Replace(Parts[2], "$1");
-                            ParsedArguments.Add(Parameter, Parts[2]);
-                        }
-                        Parameter = null;
-                        break;
+                        param = null;
+                    }
+                    else // no more identifiers, remainder is value.
+                    {
+                        parameters.Add(param, remainder.Trim());
+                        remainder = String.Empty;
+                        param = null;
+                    }
                 }
             }
-            // In case a parameter is still waiting
-            if (Parameter != null)
+
+            if (param != null)
             {
-                if (!ParsedArguments.ContainsKey(Parameter))
+                if (parameterTypes[param].PropertyType == typeof(bool))
                 {
-                    ParsedArguments.Add(Parameter, true.ToString());
+                    parameters.Add(param, true.ToString());
+                }
+                else if (parameterTypes[param].PropertyType == typeof(string))
+                {
+                    parameters.Add(param, "");
                 }
             }
+
+
+            return parameters;
         }
+
 
         public void Parse(string[] args)
         {
-            ParseArguments(args); // TODO: do this in another way.
-            SetupParameterDictionary();
+            if (args == null)
+                return;
+
+            var parameters = GetParameterDictionary();
+            var parsedVariables = ParseArguments(args, parameters);
 
             // Set values on properties for found parameters.
-            foreach (var entry in ParsedArguments)
+            foreach (var entry in parsedVariables)
             {
                 var parameterName = entry.Key;
                 var valueString = entry.Value;
 
-                if (!Parameters.ContainsKey(parameterName)) // parameter mismatch match.
+                if (!parameters.ContainsKey(parameterName)) // parameter mismatch match.
                 {
                     throw new InvalidProgramException(String.Format("Unknown parameter specified: {0}", parameterName));
                 }
@@ -209,12 +138,13 @@ namespace CommandLineParser
                 object value = null;
                 try
                 {
-                    value = ConvertStringToObject(valueString, Parameters[parameterName].PropertyType);
-                    Parameters[parameterName].SetValue(this, value, null);
+                    value = ConvertStringToObject(valueString, parameters[parameterName].PropertyType);
+                    OnPropertySetByCommandLine(parameters[parameterName], value);
+                    parameters[parameterName].SetValue(this, value, null);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
-                    throw new InvalidProgramException(String.Format("Invalid value: {0}, specified for parameter {1}", valueString, parameterName), ex);
+                    throw new Exception(String.Format("Invalid value: {0}, specified for parameter {1}", valueString, parameterName), ex);
                 }
             }
         }
@@ -224,21 +154,21 @@ namespace CommandLineParser
             return TypeDescriptor.GetConverter(type).ConvertFromString(null, CultureInfo.InvariantCulture, text);
         }
 
-        private void SetupParameterDictionary()
+        public Dictionary<string, PropertyInfo> GetParameterDictionary()
         {
-            Parameters = new Dictionary<string, PropertyInfo>();
+            var parameters = new Dictionary<string, PropertyInfo>();
 
             foreach (var prop in GetType().GetProperties())
             {
                 var attribute = (ParameterAttribute)Attribute.GetCustomAttribute(prop, typeof(ParameterAttribute));
                 if (attribute != null)
                 {
-                    Parameters.Add(attribute.ParamName, prop);
+                    foreach (var param in attribute.ParamName)
+                        parameters.Add(param, prop);
                 }
             }
+
+            return parameters;
         }
-
-
     }
 }
-
--- Tests/CommandLineParser.Tests/CommandLineParserTests.cs
+++ Tests/CommandLineParser.Tests/CommandLineParserTests.cs
@@ -6,14 +6,14 @@ using NUnit.Framework;
 using CommandLineParser;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace CommandLineParser.Tests
 {
-    [TestFixture]
+    [TestFixture, Timeout(5000)]
     public class CommandLineParserTests
     {
-
-        public class BasicCommandLineTest : Parser
+        private class BasicCommandLineTest : Parser
         {
             [ParameterAttribute("testParam")]
             public string TestParam { get; set; }
@@ -24,81 +24,157 @@ namespace CommandLineParser.Tests
             [ParameterAttribute("testInt")]
             public Int32 Int32Param { get; set; }
 
+            [ParameterAttribute("testInt64")]
+            public Int64 Int64Param { get; set; }
+
+            [ParameterAttribute("testFloat")]
+            public float FloatParam { get; set; }
+
+            [ParameterAttribute("path", "filename")]
+            public string PathParam { get; set; }
         }
 
+        private BasicCommandLineTest parser;
 
-        [Test]
-        public void TestRegex1()
+        [SetUp]
+        public void SetupEachTest()
         {
-            var regularExpression = new Regex(@"""(.*)""|'(.*)'"); // matches quoted (double or single) string.
-            var test = @"""heppas""";
-            var match = regularExpression.Match(test);
-
-            Assert.IsTrue(match.Success);
+            parser = new BasicCommandLineTest();
         }
 
-
-
-
-        [Test]
-        public void TestTokenize()
+        [Test, Explicit]
+        public void RegexTest() // Use for testing out regular expressions.
         {
-            var parser = new BasicCommandLineTest();
+            //var test = new Regex(@"((?<="")[^\""]*(?=""))", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+            var test = new Regex(@"^""([^\""]*)""", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
 
-            var result = parser.Tokenize("--heppas -was a 'ninja'");
-            Assert.AreEqual(9, result.Count);
-            Assert.AreEqual(5, result.Where(r => r.Item2 == Parser.Token.Separator).Count());
-            Assert.AreEqual(3, result.Where(r => r.Item2 == Parser.Token.Identifier).Count());
-            Assert.AreEqual(1, result.Where(r => r.Item2 == Parser.Token.String).Count());
+            var secureStoragePath = @"""hep"" ""pas""";
+
+            var match = test.Match(secureStoragePath);
+
+            if (match.Success)
+            {
+                Console.WriteLine("matched: '{0}', from pos: {1}, entire: '{2}'", match.Groups[1].Value, match.Index, match.Value);
+            }
         }
-
-
 
         [Test]
         public void ParseOneParameterWithValue()
         {
-            var parser = new BasicCommandLineTest();
-
-            parser.Parse(new string[] { "--testParam=123" });
+            parser.Parse(new string[] { "--testParam 123" });
             Assert.AreEqual("123", parser.TestParam);
+        }
+
+        [Test]
+        public void EventShouldBeFiredUponChangingPropertyByCommandLine()
+        {
+            var wait = new ManualResetEvent(false);
+
+            parser.PropertySetByCommandLine += (s, propertyChangeArgs) =>
+            {
+                if (propertyChangeArgs.Value.ToString() == "123")
+                {
+                    wait.Set();
+                }
+            };
+
+            parser.Parse(new string[] { "--testParam 123" });
+            wait.WaitOne();
+
+            Assert.AreEqual("123", parser.TestParam);
+        }
+
+        [Test]
+        public void ParseParameterValueFromString()
+        {
+            parser.Parse(new string[] { @"--testParam ""teststring""" });
+            Assert.AreEqual(@"""teststring""", parser.TestParam);
+        }
+
+        [Test]
+        public void ModifyPropertyWithTwoDifferentParams()
+        {
+            parser.Parse(new string[] { @"--path test1" });
+            Assert.AreEqual("test1", parser.PathParam);
+
+            parser.Parse(new string[] { @"--filename test2" });
+            Assert.AreEqual("test2", parser.PathParam);
+        }
+
+        [Test]
+        public void ShouldBeAbleToParseMultipleStringArguments()
+        {
+            var args = new string[] { @"--testParam ""test"" --path ""again""" };
+            parser.Parse(args);
+            Assert.AreEqual(@"""test""", parser.TestParam);
+            Assert.AreEqual(@"""again""", parser.PathParam);
+        }
+
+        [Test]
+        public void ParseInt64Param()
+        {
+            parser.Parse(new string[] { "--testInt64           1100200300400" });
+            Assert.AreEqual(1100200300400, parser.Int64Param);
+        }
+
+        [Test]
+        public void ParsePathParam()
+        {
+            parser.Parse(new string[] { "--path", "./test.bin" });
+            Assert.AreEqual("./test.bin", parser.PathParam);
+        }
+
+        [Test]
+        public void ShouldSupportEmptyStrings()
+        {
+            parser.Parse(new[] { "--testParam" });
+            Assert.AreEqual("", parser.TestParam);
         }
 
         [Test]
         public void ParseWithDifferentAssignmentOperators()
         {
-            var parser = new BasicCommandLineTest();
+            parser.Parse(new string[] { "--testParam '123'" });
+            Assert.AreEqual("'123'", parser.TestParam);
 
-            parser.Parse(new string[] { "--testParam='123'" });
-            Assert.AreEqual("123", parser.TestParam);
+            parser.Parse(new string[] { "--testParam \"456\"" });
+            Assert.AreEqual(@"""456""", parser.TestParam);
 
-            parser.Parse(new string[] { "--testParam=\"123\"" });
-            Assert.AreEqual("123", parser.TestParam);
+            parser.Parse(new string[] { "--testParam '789'" });
+            Assert.AreEqual("'789'", parser.TestParam);
 
-            parser.Parse(new string[] { "-testParam '123'" });
-            Assert.AreEqual("123", parser.TestParam);
+            parser.Parse(new string[] { " --testParam '910'" });
+            Assert.AreEqual("'910'", parser.TestParam);
 
-            parser.Parse(new string[] { " -testParam '123'" });
-            Assert.AreEqual("123", parser.TestParam);
+            parser.Parse(new string[] { "--testInt 716" });
+            Assert.AreEqual(716, parser.Int32Param);
 
-            parser.Parse(new string[] { " -testInt: 123" });
-            Assert.AreEqual(123, parser.Int32Param);
+            parser.Parse(new string[] { "--testInt 1" });
+            Assert.AreEqual(1, parser.Int32Param);
 
-            parser.Parse(new string[] { "-testInt:123" });
-            Assert.AreEqual(123, parser.Int32Param);
+            parser.Parse(new string[] { "--testInt 67 " });
+            Assert.AreEqual(67, parser.Int32Param);
 
-            parser.Parse(new string[] { "-testInt 123" });
-            Assert.AreEqual(123, parser.Int32Param);
+            parser.Parse(new string[] { "--testFloat 2.25 " });
+            Assert.AreEqual(2.25, parser.FloatParam);
 
-            parser.Parse(new string[] { "-testInt=123 " });
-            Assert.AreEqual(123, parser.Int32Param);
+            parser.Parse(new string[] { "--testFloat 026.7500 " });
+            Assert.AreEqual(026.7500, parser.FloatParam);
+        }
+
+        [Test]
+        public void ShouldBeAbleToUseMultipleArguments()
+        {
+            var args = new string[] { "--testFloat", "4.5", "--testInt", "32"};
+            parser.Parse(args);
+            Assert.AreEqual(4.5, parser.FloatParam);
+            Assert.AreEqual(32, parser.Int32Param);
         }
 
 
         [Test]
         public void ShouldBeAbleToAcceptDifferentTypes()
         {
-            var parser = new BasicCommandLineTest();
-
             parser.Parse(new string[] { "--testInt 100" });
             
             Assert.IsFalse(parser.BooleanParam);
@@ -108,23 +184,20 @@ namespace CommandLineParser.Tests
         [Test]
         public void ShouldBeAbleToParseMultipleTimes()
         {
-            var parser = new BasicCommandLineTest();
-
             Assert.IsFalse(parser.BooleanParam);
             
             parser.Parse(new string[] { "--testBool" });
             Assert.IsTrue(parser.BooleanParam);
 
-            parser.Parse(new string[] { "--testBool false" });
-            Assert.IsFalse(parser.BooleanParam);
+            parser.Parse(new string[] { "--testInt 100" });
+            Assert.AreEqual(100, parser.Int32Param);
 
         }
 
         [Test]
-        public void ShouldThrowInvalidProgramExceptionOnInvalidValueForParam()
+        public void ShouldThrowExceptionOnInvalidValueForParam()
         {
-            var parser = new BasicCommandLineTest();
-            Assert.Throws(typeof(InvalidProgramException), delegate
+            Assert.Throws(typeof(Exception), delegate
             {
                 parser.Parse(new string[] { "--testBool 123" }); // Wrong value for type bool
             });
@@ -133,8 +206,6 @@ namespace CommandLineParser.Tests
         [Test]
         public void TestOneParameterWithoutValue()
         {
-            var parser = new BasicCommandLineTest();
-
             parser.Parse(new string[] { "--testBool" });
             Assert.IsTrue(parser.BooleanParam);
         }
